3620435e576c0acade5ede879f8dac7fe2f00eea
Siarheispivak <siarheispivak2@gmail.com>
2022-02-03 01:45:40 +0300

    16

--- js/main.js
+++ js/main.js
@@ -211,3 +211,70 @@ function isDivisible(n, x, y) {
         return false;
     }
 }
+
+
+
+
+
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+
+
+
+
+
+function century(year) {
+    // Finish this :)
+    return Math.ceil(year / 100);;
+}
+
+
+
+
+
+
+
+
+
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+
+
+
+function basicOp(operation, value1, value2) {
+    // Code
+    if (operation == "+") {
+        return value1 + value2
+    }
+    if (operation == "-") {
+        return value1 - value2
+    }
+    if (operation == "*") {
+        return value1 * value2
+    }
+    if (operation == "/") {
+        return value1 / value2
+    }
+}
